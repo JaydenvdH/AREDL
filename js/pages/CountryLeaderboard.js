97590bed991a6732d3f929716b4b0072782db2b5
Mel <rjc.mast@gmail.com>
2026-04-10 19:23:25 +0200

    Pain

--- js/pages/CountryLeaderboard.js
+++ js/pages/CountryLeaderboard.js
@@ -7,106 +7,151 @@ export default {
     components: {
         Spinner,
     },
-    data: () => ({
-        leaderboard: [],
-        loading: true,
-        selected: 0,
-        err: [],
-    }),
+data: () => ({
+    leaderboard: {},
+    provinces: [],
+    selectedProvince: null,
+    loading: true,
+    selected: 0,
+    err: [],
+}),
     template: `
         <main v-if="loading">
-            <Spinner></Spinner>
-        </main>
-        <main v-else class="page-leaderboard-container">
-            <div class="page-leaderboard">
-                <div class="error-container">
-                    <p class="error" v-if="err.length > 0">
-                        Leaderboard may be incorrect, as the following levels could not be loaded: {{ err.join(', ') }}
-                    </p>
-                </div>
-                <div class="board-container">
-                    <table class="board">
-                        <tr v-for="(ientry, i) in leaderboard">
-                            <td class="rank">
-                                <p class="type-label-lg">#{{ i + 1 }}</p>
-                            </td>
-                            <td class="total">
-                                <p class="type-label-lg">{{ localize(ientry.total) }}</p>
-                            </td>
-                            <td class="user" :class="{ 'active': selected == i }">
-                                <button @click="selected = i">
-                                    <span class="type-label-lg">{{ ientry.user }}</span>
-                                </button>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="player-container">
-                    <div class="player">
-                        <h2>#{{ selected + 1 }} {{ entry.user }} - {{ entry.verified.length + entry.completed.length }} demons</h2>
-                        <h3>{{ entry.total }} points</h3>
-                        <div class="packs" v-if="entry.packs.length > 0">
-                            <div v-for="pack in entry.packs" class="tag" :style="{background:pack.colour, color:getFontColour(pack.colour)}">
-                                {{pack.name}}
-                            </div>
-                        </div>
-                        <h2 v-if="entry.verified.length > 0">First Victor ({{ entry.verified.length}})</h2>
-                        <table class="table">
-                            <tr v-for="score in entry.verified">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
-                        <h2 v-if="entry.completed.length > 0">Completed ({{ entry.completed.length }})</h2>
-                        <table class="table">
-                            <tr v-for="score in entry.completed">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
-                        <h2 v-if="entry.progressed.length > 0">Progressed ({{entry.progressed.length}})</h2>
-                        <table class="table">
-                            <tr v-for="score in entry.progressed">
-                                <td class="rank">
-                                    <p>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.percent }}% {{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
+    <Spinner></Spinner>
+</main>
+<main v-else class="page-leaderboard-container">
+    <div class="page-leaderboard">
+        <div class="error-container">
+            <p class="error" v-if="err.length > 0">
+                Leaderboard may be incorrect, as the following levels could not be loaded: {{ err.join(', ') }}
+            </p>
+        </div>
+
+        <!-- Province Tabs !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+        <div class="province-tabs">
+            <button
+                v-for="province in provinces"
+                :key="province"
+                @click="selectedProvince = province; selected = 0"
+                :class="{ active: selectedProvince === province }"
+            >
+                {{ province }}
+            </button>
+        </div>
+
+        <div class="board-container">
+            <table class="board">
+                <tr v-for="(ientry, i) in entries">
+                    <td class="rank">
+                        <p class="type-label-lg">#{{ i + 1 }}</p>
+                    </td>
+                    <td class="total">
+                        <p class="type-label-lg">{{ localize(ientry.total) }}</p>
+                    </td>
+                    <td class="user" :class="{ 'active': selected == i }">
+                        <button @click="selected = i">
+                            <span class="type-label-lg">{{ ientry.user }}</span>
+                        </button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="player-container" v-if="entry">
+            <div class="player">
+                <h2>
+                    #{{ selected + 1 }} {{ entry.user }} - 
+                    {{ entry.verified.length + entry.completed.length }} demons
+                </h2>
+                <h3>{{ entry.total }} points</h3>
+
+                <div class="packs" v-if="entry.packs.length > 0">
+                    <div
+                        v-for="pack in entry.packs"
+                        class="tag"
+                        :style="{background:pack.colour, color:getFontColour(pack.colour)}"
+                    >
+                        {{ pack.name }}
                     </div>
                 </div>
+
+                <h2 v-if="entry.verified.length > 0">
+                    First Victor ({{ entry.verified.length}})
+                </h2>
+                <table class="table">
+                    <tr v-for="score in entry.verified">
+                        <td class="rank">
+                            <p>#{{ score.rank }}</p>
+                        </td>
+                        <td class="level">
+                            <a class="type-label-lg" target="_blank" :href="score.link">
+                                {{ score.level }}
+                            </a>
+                        </td>
+                        <td class="score">
+                            <p>+{{ localize(score.score) }}</p>
+                        </td>
+                    </tr>
+                </table>
+
+                <h2 v-if="entry.completed.length > 0">
+                    Completed ({{ entry.completed.length }})
+                </h2>
+                <table class="table">
+                    <tr v-for="score in entry.completed">
+                        <td class="rank">
+                            <p>#{{ score.rank }}</p>
+                        </td>
+                        <td class="level">
+                            <a class="type-label-lg" target="_blank" :href="score.link">
+                                {{ score.level }}
+                            </a>
+                        </td>
+                        <td class="score">
+                            <p>+{{ localize(score.score) }}</p>
+                        </td>
+                    </tr>
+                </table>
+
+                <h2 v-if="entry.progressed.length > 0">
+                    Progressed ({{entry.progressed.length}})
+                </h2>
+                <table class="table">
+                    <tr v-for="score in entry.progressed">
+                        <td class="rank">
+                            <p>#{{ score.rank }}</p>
+                        </td>
+                        <td class="level">
+                            <a class="type-label-lg" target="_blank" :href="score.link">
+                                {{ score.percent }}% {{ score.level }}
+                            </a>
+                        </td>
+                        <td class="score">
+                            <p>+{{ localize(score.score) }}</p>
+                        </td>
+                    </tr>
+                </table>
             </div>
-        </main>
+        </div>
+    </div>
+</main>
     `,
     computed: {
+        entries() {
+            return this.leaderboard[this.selectedProvince] || [];
+        },
         entry() {
-            return this.leaderboard[this.selected];
+            return this.entries[this.selected];
         },
     },
     async mounted() {
         const [leaderboard, err] = await fetchCountryLeaderboard();
+
         this.leaderboard = leaderboard;
+        this.provinces = Object.keys(leaderboard);
+        this.selectedProvince = this.provinces[0];
+
         this.err = err;
-        // Hide loading spinner
         this.loading = false;
     },
     methods: {
