4c897e9fe6faff792eb07057f9ffbf0f5787f299
Mel <rjc.mast@gmail.com>
2026-04-11 19:50:14 +0200

    beta page fixes

--- js/pages/betaInfo.js
+++ js/pages/betaInfo.js
@@ -8,33 +8,33 @@ export default {
         err: [],
     }),
     template: `
-        <main>
-                <h1>Beta Info</h1>
+    <main>
+        <div class="beta-page">
+            <h1>Beta Info</h1>
 
-                <p>Hi, Mel here.</p> 
-                <p>
-                    I'm working on DDL 2.0 right now, and I'd like you guys to be able
-                    to access those pages beforehand for testing purposes.
-                </p>
+            <p>Hi, Mel here.</p>
 
-                <p>Here's the updated list page:</p>
-                <router-link to="/countrylist">
-                    <button>CountryList</button>
-                </router-link>
+            <p>
+                I'm working on DDL 2.0 right now, and I'd like you guys to be able
+                to access those pages beforehand for testing purposes.
+            </p>
 
-                <p>And here's the updated leaderboard page:</p>
-                <router-link to="/countryleaderboard">
-                    <button>CountryLeaderboard</button>
-                </router-link>
+            <p>Here's the updated list page:</p>
+            <router-link to="/countrylist">
+                <button>CountryList</button>
+            </router-link>
 
-                <p>Happy browsing!</p>
+            <p>And here's the updated leaderboard page:</p>
+            <router-link to="/countryleaderboard">
+                <button>CountryLeaderboard</button>
+            </router-link>
 
-                <p>
-                    <small>
-                        Oh btw, DM me if u find a bug. @melted.nl ofc.
-                    </small>
-                </p>
-            </div>
-        </main>
-    `
+            <p>Happy browsing!</p>
+
+            <p>
+                <small>Oh, btw, DM me if u find a bug. @melted.nl ofc.</small>
+            </p>
+        </div>
+    </main>
+`
 }
